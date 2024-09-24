751df9fa5514c79e898d30c2279376b8b586bf95
ina <c0b23017d4@edu.teu.ac.jp>
2024-09-24 17:14:16 +0900

    練習１と３　背景画像の描写

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -5,28 +5,21 @@ import pygame as pg
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
-    pg.display.set_caption("はじめてのPygame")
+    pg.display.set_caption("羽ばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock = pg.time.Clock()
-    font = pg.font.Font(None, 80)
-
-    enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
-    enn.set_colorkey((0, 0, 0))
-
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
-        txt = font.render(str(tmr), True, (255, 255, 255))
-        screen.fill((50, 50, 50))
-        screen.blit(txt, [300, 200])
-        screen.blit(enn, [100, 400])
 
+        screen.blit(bg_img,[0,0])
         pg.display.update()
         tmr += 1        
-        clock.tick(0.1)
+        clock.tick(10)
 
 if __name__ == "__main__":
     pg.init()
