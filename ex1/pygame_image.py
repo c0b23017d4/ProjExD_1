77d430833fea0fbb55495ce86fb75d01bec80290
ina <c0b23017d4@edu.teu.ac.jp>
2024-09-24 17:05:15 +0900

    FPS変更(2)

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -4,29 +4,29 @@ import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
-    pg.display.set_caption("はばたけ！こうかとん")
+    pg.display.set_caption("はじめてのPygame")
     screen = pg.display.set_mode((800, 600))
-    clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    clock = pg.time.Clock()
+    font = pg.font.Font(None, 80)
 
-    kk_img = pg.image.load("fig/3.png")
-    kk_img = pg.transform.flip(kk_img,True,False)
+    enn = pg.Surface((20, 20))
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    enn.set_colorkey((0, 0, 0))
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        
+        txt = font.render(str(tmr), True, (255, 255, 255))
+        screen.fill((50, 50, 50))
+        screen.blit(txt, [300, 200])
+        screen.blit(enn, [100, 400])
 
-        x = -(tmr % 800)
-
-        screen.blit(bg_img, [x, 0])
-        screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
-
+        clock.tick(0.1)
 
 if __name__ == "__main__":
     pg.init()
