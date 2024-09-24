6fe1505bea1f2f83b18bb3a24692b8483cad320a
ina <c0b23017d4@edu.teu.ac.jp>
2024-09-24 18:08:14 +0900

    完全版

--- ex1/japanese_font.py
+++ ex1/japanese_font.py
@@ -4,28 +4,24 @@ import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+
 def main():
-    pg.display.set_caption("はじめてのPygame")
+    font_lst = [f for f in pg.font.get_fonts() if f != ""]
+    print(font_lst)
+
     screen = pg.display.set_mode((800, 600))
     clock = pg.time.Clock()
-    font = pg.font.Font(None, 80)
-
-    enn = pg.Surface((20, 20))
-    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
-    enn.set_colorkey((0, 0, 0))
-
-    tmr = 0
+    font = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 30)
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
-        txt = font.render(str(tmr), True, (255, 255, 255))
-        screen.fill((50, 50, 50))
+        txt = font.render("こんにちは世界", True, (255, 255, 255))
+        screen.fill((0, 0, 0))
         screen.blit(txt, [300, 200])
-        screen.blit(enn, [100, 400])
         pg.display.update()
-        tmr += 1        
-        clock.tick(0.1)
+        clock.tick(10)
+
 
 if __name__ == "__main__":
     pg.init()
