42f737790ade3ebb68be7ed969944c50abfdf177
ina <c0b23017d4@edu.teu.ac.jp>
2024-09-24 17:56:44 +0900

    演習2

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -26,25 +26,29 @@ def main():
 
         key_lst = pg.key.get_pressed() 
 
+        move_x = -1
+        move_y = 0
+
         kk_dx = -1
 
         if key_lst[pg.K_UP]: 
-            kk_rct.move_ip((0, -1)) 
+            move_y = -1
         if key_lst[pg.K_DOWN]:  
-            kk_rct.move_ip((0, +1))  
+            move_y = 1
         if key_lst[pg.K_LEFT]: 
-            kk_rct.move_ip((-1, 0))  
+            move_x = -1   
         if key_lst[pg.K_RIGHT]:  
-            kk_dx = 2 
+            move_x = 2
 
-        
+        kk_rct.move_ip(move_x, move_y)
+
         x = -(tmr%3200)
         screen.blit(bg_img,[x,0])
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img2, [x+4800, 0])
-        
-        kk_rct.move_ip(kk_dx, 0)
+
+        #kk_rct.move_ip(kk_dx, 0)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
